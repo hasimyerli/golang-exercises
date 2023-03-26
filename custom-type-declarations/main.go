f8057a75e59dc29d6cfb845c5a049fab90338e7e
phasimyerli <hasim.yerli@picasso.run>
2023-03-26 08:52:13 +0300

    implemente  function for string type

--- custom-type-declarations/main.go
+++ custom-type-declarations/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type StringList []string
 
+// Checks if a value exists in an list
 func (s StringList) Contains(v string) bool {
 	for _, i := range s {
 		if i == v {
@@ -14,6 +18,16 @@ func (s StringList) Contains(v string) bool {
 	return false
 }
 
+// Join list elements with a string
+func (l StringList) Implode(separator string) string {
+	s := ""
+	for _, i := range l {
+		s += i + separator
+	}
+
+	return strings.TrimSuffix(s, separator)
+}
+
 var fruits StringList
 
 func init() {
@@ -21,7 +35,11 @@ func init() {
 }
 
 func main() {
+	//Example-1
 	fmt.Println(fruits.Contains("apple")) //true
 	fmt.Println(fruits.Contains("grape")) //true
 	fmt.Println(fruits.Contains("pear"))  //false
+
+	//Example-2
+	fmt.Println(fruits.Implode(";")) // "apple;grape;pear"
 }
